8cf2cd22f0df9c0115dfa3ab37ebae06ef823fbf
Tassos Bareiss <tassosbareiss@gmail.com>
2017-01-31 22:59:18 -0500

    Add to explanation of min-heap

--- src/js_assessment.js
+++ src/js_assessment.js
@@ -154,44 +154,88 @@ const transpose = function (arr) {
 };
 
 
+// Heaps are data structures that allow for constant time lookup of minimum
+// and maximum, in the case of min heaps and max heaps, respectively.
+// In other words, given an array of numbers translated to a min heap, you
+// will always be able to find the minimum in constant (O(1)) time.
+// As a bonus challenge, we will be implementing a min heap with the ability to
+// add elements in log(n) time.
+//
+// In order to do so, we will need a MinHeap class, and a MinHeap.prototype.add(val)
+// instance method that adds elements in log(n) time.
+//
+// The only rule for a minimum heap is that no node has a parent
+// that is larger than it - tho parents can be the same as children (just not larger).
+//
+// The hard part will be adding elements. Min heaps are binary trees that
+// minimize depth - this means that each generation is filled from left
+// to right before a new generation is started. An example will make this more
+// clear:
+//
+//                      3
+//                    /   \
+//                   5      3
+//                 /  \    /
+//                7    9  6
+//
+// This implementation is correct - we would not add a child to 7 before 3
+// because the second generation is not yet full (from left to right).
+//
+// The way we add an element is, you find the NEXT place an element should be added
+// (in our example, to the right of 6), and place the new element there.
+//
+// And then swap the element with its parents until it is either greater than or
+// equal to its parent.
+//
+// So, if we were to add '1' to this example, it would swap with its first parent
+// (3), and then again with its new parent (3).
+//
+// However, if we were to add a '5', no swapping would occur.
+
+// An convenient implementation of a min-heap is with an array that stores allow
+// of the elements ordered by their position in the tree. In this ordering, the
+// first element (at index 0) is the root minimum element. For the above tree,
+// the array representation would be so:
+//      [1, 5, 3, 7, 8, 6]
+//  idx: 0  1  2  3  4  5
+
+// The ordering starts at the top, then traverses each row of the tree from left to
+// right (like a BFS). As you can see, to add a new element, we simply need to
+// push the element onto the array.
+
+// Additionally, the parent of an element at a given index i is always the element
+// at floor(i / 2). For example, the parent of 7 (index 3) is 5 (index 3 / 2 = 1).
+// The exception is the element at index 0, which has no parent.
+//
+// For this problem, you should have a class for MinHeap,
+// the ability to find the minimum immediately,
+// and the ability to add elements.
+//
+// You will need a helper method, _swap, which swaps two elements in the tree.
+//
+
 const MinHeap = function () {
   this.heap = [];
 };
 
 MinHeap.prototype.getMin = function () {
-  return this.heap[0];
+
 };
 
+//#add must push in the given node value and then perform the
+//necessary swapping until the value of that node's parent
+//is less than that node's value (or the node is at the front
+//of our heap)
 MinHeap.prototype.add = function (value) {
-  let childIdx = this.heap.length;
-  this.heap.push(value);
-  let parentIdx = MinHeap.getParentIdx(childIdx);
-  let parentVal = this.heap[parentIdx];
 
-  while (value < parentVal) {
-    if (childIdx === 0) { break; }
-
-    this._swap(childIdx, parentIdx);
-
-    childIdx = parentIdx;
-    parentIdx = MinHeap.getParentIdx(childIdx);
-    parentVal = this.heap[parentIdx];
-  }
 };
 
+//The index of the child's parent will always be the floor of
+//half its index.
 MinHeap.getParentIdx = function (childIdx) {
   return Math.floor(childIdx / 2);
 };
 
-MinHeap.prototype.getParent = function(idx) {
-  if (idx === 0) { return null; }
-
-  return this.heap[Math.floor(idx / 2)];
-};
-
 MinHeap.prototype._swap = function (childIdx, parentIdx) {
-  const childVal = this.heap[childIdx];
 
-  this.heap[childIdx] = this.heap[parentIdx];
-  this.heap[parentIdx] = childVal;
 };
